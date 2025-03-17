91fb6ccb38121d3407858db9a5d72562172ea1bc
Ghassen loukil <ghassen.loukil.it@gmail.com>
2025-03-17 15:58:22 +0000

    add server certificate

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,13 +3,26 @@ import { WebSocketServer, WebSocket } from "ws";
 import { checkGameProgress, setupContract } from "./zkAppHandler.js";
 import { StepProgramProof } from "@navigators-exploration-team/mina-mastermind";
 import { getGame, saveGame } from "./kvStorageService.js";
+import https from "https";
+import http from "http";
+import fs from "fs";
 
 const app = express();   
 const PORT = 3000;    
 setupContract();
-const server = app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+const useHttps = process.env.USE_HTTPS === 'true';
+
+let server;
+if (useHttps) {
+  const sslOptions = {
+    key: fs.readFileSync("/app/certs/server.key"),
+    cert: fs.readFileSync("/app/certs/server.crt"),
+  };
+  server = https.createServer(sslOptions, app);
+} else {
+  server = http.createServer(app);
+}
+
 
 const wss = new WebSocketServer({ server });
 
@@ -86,4 +99,9 @@ wss.on("connection", (ws) => {
   ws.on("error", (err) => {
     console.error("WebSocket error:", err);
   });
+});
+
+server.listen(PORT, () => {
+  const protocol = useHttps ? "https" : "http";
+  console.log(`Server is running on ${protocol}://localhost:${PORT}`);
 });
